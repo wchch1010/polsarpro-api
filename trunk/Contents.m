842f45ff25d288c0fce6ad05eb6e064d66ffb39d
samuel.foucher@gmail.com <samuel.foucher@gmail.com@42d77660-977d-e5e0-23ea-de1f2e8b7d61>
2011-06-11 01:01:54 +0000

    First version

---
+++ trunk/Contents.m
@@ -0,0 +1,23 @@
+% PolSARPro Matlab API
+% Version 1.0, June 2011
+% 
+% see http://code.google.com/p/polsarpro-api/ for details
+% 
+% PolSARPro Matlab Api Toolbox comes with ABSOLUTELY NO WARRANTY; for details
+% see License.txt in the program package. This is free software,
+% and you are welcome to redistribute it under certain conditions;
+% see License.txt for details.
+% 
+% 
+% Demos
+% 
+%            test_filers
+% 
+% Speckle Filters
+% 
+%            pspLeeSigmaFilter 
+%            pspRefiendLeeFilter
+% 
+% Other
+% 
+
