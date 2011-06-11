0afb51d24e34ef95bad6487c223325eb88c696fd
samuel.foucher <samuel.foucher@gmail.com>
2011-06-11 21:04:56 +0000

    added Yamaguchi

--- tests/test_model_based_decomposition.m
+++ tests/test_model_based_decomposition.m
@@ -1,14 +1,15 @@
 function test_model_based_decomposition
-% test read and write functions
+% test model based decompositions
 close all;
 clc;
 disp('Start test_model_based_decomposition');
 load 'test.mat';
 st.info.NLooks= 1;
-[stOUT]= pspFreemanDecomposition(st, 5);
-[RGB0]= pspCreatePauliRGBFile(st);
-
-figure(1),imshow(RGB0,[]);
-figure(2),imagesc([stOUT.data(:,:,1)]);
-
+[stF]= pspLeeRefinedFilter(st, 1, 9);
+[stOUT]= pspFreemanDecomposition(stF);
+figure('Name','pspFreemanDecomposition'),imagesc([stOUT.data(:,:,1) stOUT.data(:,:,2) stOUT.data(:,:,3)]);axis off image;colorbar;
+[stOUT]= pspYamaguchiDecomposition(stF, 3);
+figure('Name','pspYamaguchiDecomposition3'),imagesc([stOUT.data(:,:,1) stOUT.data(:,:,2) stOUT.data(:,:,3)]);axis off image;colorbar;
+[stOUT]= pspYamaguchiDecomposition(stF, 4);
+figure('Name','pspYamaguchiDecomposition4'),imagesc([stOUT.data(:,:,1) stOUT.data(:,:,2) stOUT.data(:,:,3) stOUT.data(:,:,4)]);axis off image;colorbar;
 
