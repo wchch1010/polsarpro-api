b4921e16dac67d604589e4eb1d8e604d18f76f70
samuel.foucher <samuel.foucher@gmail.com>
2011-06-11 14:53:30 +0000

    removed the use of ColorComposite function

--- tests/test_pspCloudeDecomposition.m
+++ tests/test_pspCloudeDecomposition.m
@@ -5,6 +5,11 @@ clc;
 disp('Start test_read_write');
 load 'test.mat';
 [stOUT]= pspCloudeDecomposition(st, 7);
-[RGB0]= ColorComposite(st,[1 4 6],0.05,1);
-[RGB1]= ColorComposite(stOUT,[1 4 6],0.05,1);
-figure,imshow(cat(2,RGB0,RGB1),[]);
+[RGB0]= pspCreatePauliRGBFile(st);
+[RGB1]= pspCreatePauliRGBFile(stOUT);
+RGB0= cat(2,RGB0,RGB1);
+figure('Name','pspCloudeDecomposition'),imshow(RGB0,[]);
+
+[stOut]= pspHAAlphaDecomposition(stOUT, 7);
+RGB0= [stOut.data(:,:,1) stOut.data(:,:,2) stOut.data(:,:,3)./90.0];
+figure('Name','pspHAAlphaDecomposition'),imagesc(RGB0);axis image off; colorbar;
