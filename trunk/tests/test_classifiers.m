e6bf470549e5faa6d5569acaf244f5782d2ed044
samuel.foucher@gmail.com <samuel.foucher@gmail.com@42d77660-977d-e5e0-23ea-de1f2e8b7d61>
2011-06-12 14:41:18 +0000

    add new functions

--- trunk/tests/test_classifiers.m
+++ trunk/tests/test_classifiers.m
@@ -4,10 +4,13 @@ close all;
 clc;
 disp('Start test_classifiers');
 load 'test.mat';
-[stF]= pspLeeRefinedFilter(st, 1, 9);
-[stCL]= pspWishartHAAlphaClassifier(stF)
+stF= pspLeeRefinedFilter(st, 1, 9); %-- filtering first
+stHAAlpha= pspHAAlphaDecomposition(stF);  %-- Compute H/A/Alpha decomposition
+stCL= pspWishartHAAlphaClassifier(stF, stHAAlpha); %-- Compute Wishart classifications
 RGB0= [stCL.data(:,:,1) stCL.data(:,:,2)];
 figure('Name','pspWishartHAAlphaClassifier'),imagesc(RGB0);axis image off; colorbar;
-[stCL]= pspHAAlphaPlanesClassifier(stF);
+%[stOut]= pspClassificationColormapPauli(stF,stCL,'Wishart_Pauli.bmp'); %-- does not work for now
+
+stCL= pspHAAlphaPlanesClassifier(stHAAlpha);  %-- Compute simple H/A/alpha classifications
 RGB0= [stCL.data(:,:,1) stCL.data(:,:,2) stCL.data(:,:,3)];
-figure('Name','pspHAAlphaPlanesClassifier'),imagesc(RGB0);axis image off; colorbar;
+figure('Name','pspHAAlphaPlanesClassifier'),imagesc(RGB0);axis image off; colorbar;title('H/A       H/alpha       A/alpha');
