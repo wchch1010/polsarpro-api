b4c09a0ee4538d78f0a837c0ad7e1ec788ab5231
samuel.foucher <samuel.foucher@gmail.com>
2011-06-12 01:01:17 +0000

    first commit

---
+++ index_pspapi.m
@@ -0,0 +1,42 @@
+% PolSARPro Matlab API
+% Version 1.0, June 2011
+% 
+% see http://code.google.com/p/polsarpro-api/ for details
+% 
+% PolSARPro Matlab Api Toolbox comes with ABSOLUTELY NO WARRANTY; for details
+% see License.txt in the program package. This is free software,
+% and you are welcome to redistribute it under certain conditions;
+% see License.txt for details.
+% 
+% 
+% Demos
+% 
+%            test_filters
+% 
+% Speckle Filters
+% 
+%            pspLeeSigmaFilter 
+%            pspRefinedLeeFilter
+%            pspPWFFilter
+%
+% Model based decomposition
+%
+%            pspFreemanDecomposition
+%            pspYamaguchiDecomposition
+%
+% Eigenvector based decomposition
+%
+%            pspHAAlphaDecomposition
+%
+% Classifier
+% 
+%            pspWishartHAAlphaClassifier
+%            pspHAAlphaPlanesClassifier
+%
+% Visualization
+%
+%            pspCreatePauliRGBFile
+% Other
+% 
+
+help index_pspapi.m
