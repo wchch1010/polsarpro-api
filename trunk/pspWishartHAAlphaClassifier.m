88c6e4a59e483d7debe1c8603fdfd3ee0af29337
samuel.foucher@gmail.com <samuel.foucher@gmail.com@42d77660-977d-e5e0-23ea-de1f2e8b7d61>
2011-06-12 14:42:52 +0000

    HAalpha parameters now provided as input

--- trunk/pspWishartHAAlphaClassifier.m
+++ trunk/pspWishartHAAlphaClassifier.m
@@ -1,29 +1,30 @@
-function [stOut]= pspWishartHAAlphaClassifier(stIn, pct_min,nb_it_max,Bmp_flag)
+function [stOut]= pspWishartHAAlphaClassifier(stIn, stHAAlpha, pct_min,nb_it_max,Bmp_flag)
 % Synopsis:
-%  [stOut]= pspWishartHAAlphaClassifier(stIn, pct_min,nb_it_max,Bmp_flag)
+%  [stOut]= pspWishartHAAlphaClassifier(stIn, stHAAlpha, pct_min,nb_it_max,Bmp_flag)
 % 
 % Input:
 % - stIn     structure  .data
 %                       .info(.name, .type)
-% 
+% - stHAAlpha  structure containing H/A/Alpha data
+%
 % Output:
-% - stOut     matrix with 2 channels (8 and 16 classe classification)
+% - stOut     matrix with 2 channels (8 and 16 classe classification, wishart_H_A_alpha_class and wishart_H_alpha_class)
 %
 % Description:
 % 
-%  
 % PolSARPro Description : Unsupervised maximum likelihood classification of a polarimetric image from the Wishart PDF of its coherency matrices. Two classsifcation are avaliable :
 % • Initialisation using the H and alpha parameters -> 8 classes
 % • Same classification followed by another initialisation using the anisotropy A -> 16 classes
 %
-% Note: prior filtering is required
+% Note: prior filtering is required and the computation of H/A/alpha
+% decomposition (see pspHAAlphaDecomposition)
 %
 % See also
 %
 % Revisions:
 %   S. Foucher: initial version (2011/11/06)
 %
-error(nargchk(1,4,nargin, 'struct'));
+error(nargchk(2,5,nargin, 'struct'));
 error(nargoutchk(0,1,nargout, 'struct'));
 
 global POLSARPRO_API_OUT_DIR;
@@ -40,16 +41,17 @@ if ~exist('nb_it_max')
     nb_it_max= 10;
 end 
 if ~exist('Bmp_flag')
-    Bmp_flag= 0;
+    Bmp_flag= 0; %-- value 1 does not work
 end
 sColorMapWishart8= [POLSARPRO_DIR '/ColorMap/ColorMapWishart8.pal'];
 sColorMapWishart16= [POLSARPRO_DIR '/ColorMap/ColorMapWishart16.pal'];
-options= parseInputs(stIn, NwinFilter, pct_min, nb_it_max, Bmp_flag);
-stOut= pspHAAlphaDecomposition(stIn, 1); %-- H/A/alpha decomposition required first
+options= parseInputs(stIn, stHAAlpha, NwinFilter, pct_min, nb_it_max, Bmp_flag);
+%stOut= pspHAAlphaDecomposition(stIn, 1); %-- H/A/alpha decomposition required first
 stOut= [];
 % Convert matlab data to polSARpro data
 mat2psp(stIn);
 stHeaderNames= mat2psp(stIn, POLSARPRO_API_OUT_DIR, 0); % necessary in order to create header in the OUT dir
+mat2psp(stHAAlpha);
 
 [numRow, numCol, nBands] = size(stIn.data);
 offsetRow = 0;
@@ -61,7 +63,7 @@ h = waitbar(0,'H/A/Alpha Wishart Classification in progress, please wait...');
 %OffsetCol FinalNrow FinalNcol pct_min nb_it_max Bmp_flag ColorMapWishart8 ColorMapWishart16
 sCommand = sprintf('%s/Soft/data_process_sngl/wishart_h_a_alpha_classifier_%s.exe',POLSARPRO_DIR,upper(stIn.info.type));
 sCommand= sprintf('"%s" "%s" "%s" %f %d %d %d %d %f %f %d "%s" "%s" "%s" "%s" "%s"',sCommand,POLSARPRO_API_IN_DIR,POLSARPRO_API_OUT_DIR,1,0,0,numRow,numCol,...
-    options.pct_min,options.nb_it_max,options.Bmp_flag,sColorMapWishart8,sColorMapWishart16,[POLSARPRO_API_OUT_DIR '/entropy.bin'],[POLSARPRO_API_OUT_DIR '/anisotropy.bin'],[POLSARPRO_API_OUT_DIR '/alpha.bin']);
+    options.pct_min,options.nb_it_max,options.Bmp_flag,sColorMapWishart8,sColorMapWishart16,[POLSARPRO_API_IN_DIR '/entropy.bin'],[POLSARPRO_API_IN_DIR '/anisotropy.bin'],[POLSARPRO_API_IN_DIR '/alpha.bin']);
 
 waitbar(0.3);
 
@@ -74,6 +76,9 @@ if isempty(vProgress)
 end
 stOut= psp2mat('WISHART_HAALPHA',POLSARPRO_API_OUT_DIR,[POLSARPRO_API_IN_DIR '/' stHeaderNames{1}]);
 stOut.info.type= 'WISHART_HAALPHA';
+if Bmp_flag
+    
+end
 if isfield(stIn.info,'history')
     stOut.info.history= stIn.info.history;
     stOut.info.history{end+1}= 'pspWishartHAAlphaClassifier';
@@ -85,7 +90,7 @@ waitbar(1);
 close(h)
 
 %=============================================================
-function options = parseInputs(st, NwinFilter, pct_min, nb_it_max, Bmp_flag)
+function options = parseInputs(st, stHAAlpha, NwinFilter, pct_min, nb_it_max, Bmp_flag)
 
 if ~isnumeric(NwinFilter)
   error('MATLAB:pspWishartHAAlphaClassifier:badData', ...
@@ -98,6 +103,14 @@ end
 if ~isfield(st.info,'type')
   error('MATLAB:pspWishartHAAlphaClassifier:badData', ...
         'DATA type missing.');
+end
+if ~isfield(stHAAlpha.info,'type')
+  error('MATLAB:pspWishartHAAlphaClassifier:badData', ...
+        'DATA type missing.');
+end
+if ~strcmpi(stHAAlpha.info.type,'HAALPHA')
+  error('MATLAB:pspWishartHAAlphaClassifier:badData', ...
+        'Input argument 2 must be of HAALPHA type.');
 end
 if ~isnumeric(pct_min)
   error('MATLAB:pspWishartHAAlphaClassifier:badData', ...
