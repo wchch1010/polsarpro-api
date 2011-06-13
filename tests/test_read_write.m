efde212a4c94c04eb6fec729a540fccdb8ec37eb
samuel.foucher <samuel.foucher@gmail.com>
2011-06-13 00:56:24 +0000

    DataConversion

--- tests/test_read_write.m
+++ tests/test_read_write.m
@@ -11,3 +11,10 @@ mat2psp(st, base_dir);
 disp('Loading test set');
 st2= psp2mat('T3',base_dir);
 figure,imagesc(sum(st2.data(:,:,[1 4 6]),3)); axis off square;
+disp('Data Conversion');
+[RGB0]= pspCreatePauliRGBFile(st);
+[stC3]= pspDataConversion(st,'C3'); %-- conversion toward C3
+[RGB1]= pspCreatePauliRGBFile(stC3);
+figure,imagesc([st.data(:,:,1)-stC3.data(:,:,1)]);
+[stT3]= pspDataConversion(stC3,'T3'); %-- conversion back
+sum(st.data(:)-stT3.data(:))
